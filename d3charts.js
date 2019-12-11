00ced4fe0c0c7b231d130cbf1656a869582b6ba1
Dorrie Tang <dorrie.tang@gmail.com>
2019-12-11 14:31:46 -0500

    blacken dest msas upon mouseover

--- d3charts.js
+++ d3charts.js
@@ -1,5 +1,7 @@
 const map_url = "https://raw.githubusercontent.com/erickawu/scroll_narrative/master/msa_map.json";
 const data_url = "https://raw.githubusercontent.com/erickawu/scroll_narrative/master/msa_data.json";
+const coords_url = "https://raw.githubusercontent.com/erickawu/scroll_narrative/master/msa_coords.json";
+const connections_url = "https://raw.githubusercontent.com/erickawu/scroll_narrative/master/msa_connections.json";
 
 var path = d3.geoPath(d3.geoEquirectangular());
 
@@ -17,47 +19,95 @@ function map() {
 
     let g = svg.append("g");
 
-    d3.json(map_url).then(function(MSA) {
-        d3.json(data_url).then(function(msa_data) {
-            // Bind metro paths
-            g.selectAll("path")
-            .data(topojson.feature(MSA, MSA.objects.cb_2014_us_cbsa_500k).features)
-            .enter().append("path")
-            .attr("d", path)
-            .style("fill", function (d) {
-                if (msa_data.hasOwnProperty(d.properties.GEOID)) {
-                    return "lightgray";
-                } else {
-                    return "white";
-                }
-            })
-            .on("mouseover", function(d) {
-                d3.select(this).style("fill", function (d) {
-                    if (msa_data.hasOwnProperty(d.properties.GEOID) && msa_data[d.properties.GEOID].hasOwnProperty("net")) {
-                        return d3.interpolatePiYG((msa_data[d.properties.GEOID]["net"] + 149227) / 298454);
-                    } else {
+    d3.json(map_url).then(function(map) {
+        d3.json(data_url).then(function(data) {
+            d3.json(coords_url).then(function(coords) {
+                d3.json(connections_url).then(function(conns) {
+                    // Bind metro paths
+                    g.selectAll("path")
+                    .data(topojson.feature(map, map.objects.cb_2014_us_cbsa_500k).features)
+                    .enter().append("path")
+                    .attr("d", path)
+                    .style("fill", function (d) {
+                        if (data.hasOwnProperty(d.properties.GEOID)) {
+                            return "lightgray";
+                        } else {
+                            return "white";
+                        }
+                    })
+                    .on("mouseover", function(d) {
+                        // failing attempt to draw lines from source to dests
+                        // for (var i in conns[d.properties.GEOID]) {
+                        //     if (conns[d.properties.GEOID][i]["dest"] in coords) {
+                        //         // draw line
+                        //         svg.append("line")
+                        //         .attr("x1", coords[d.properties.GEOID][0])
+                        //         .attr("y1", coords[d.properties.GEOID][1])
+                        //         .attr("x2", coords[conns[d.properties.GEOID][i]["dest"]][0])
+                        //         .attr("y2", coords[conns[d.properties.GEOID][i]["dest"]][1])
+                        //         .attr("stroke-width", 1)
+                        //         // .attr("stroke-width", parseInt(conns[d.properties.GEOID][i]["value"]))
+                        //         .attr("stroke", "black");
+                        //     }
+                        // }
+                        // blacken dests
+                        d3.selectAll('path').style("fill", function (e) {
+                            for (var i in conns[d.properties.GEOID]) {
+                                if (conns[d.properties.GEOID][i]["dest"] in coords) {
+                                    if (e.properties.GEOID == conns[d.properties.GEOID][i]["dest"]) {
+                                        return "black";
+                                    }
+                                }
+                            }
+                            if (data.hasOwnProperty(e.properties.GEOID)) {
+                                return "lightgray";
+                            } else {
+                                return "white";
+                            }
+                        });
+                        // color curr msa by net
+                        d3.select(this).style("fill", function (d) {
+                            if (data.hasOwnProperty(d.properties.GEOID) && data[d.properties.GEOID].hasOwnProperty("net")) {
+                                return d3.interpolatePiYG((data[d.properties.GEOID]["net"] + 149227) / 298454);
+                            } else {
+                                return "white";
+                            }
+                        });
+                        // show tooltip
+                        if (data.hasOwnProperty(d.properties.GEOID)) {
+                            return tooltip.style("visibility", "visible").text(data[d.properties.GEOID]["name"] + "\n" + data[d.properties.GEOID]["net"]);
+                        }
+                    })
+                    .on("mousemove", function(){
+                        return tooltip.style("top", (d3.event.pageY-10)+"px").style("left",(d3.event.pageX+10)+"px");
+                    })
+                    .on("mouseout", function(d) {
+                        d3.selectAll("line").remove();
+                        d3.select(this).style("fill", function (d) {
+                            if (data.hasOwnProperty(d.properties.GEOID)) {
+                                return "lightgray";
+                            } else {
+                                return "white";
+                            }
+                        });
+                        for (var i in conns[d.properties.GEOID]) {
+                            if (conns[d.properties.GEOID][i]["dest"] in coords) {
+                                // stroke around dests
+                                d3.selectAll('path').style("fill", function (e) {
+                                    if (data.hasOwnProperty(e.properties.GEOID)) {
+                                        return "lightgray";
+                                    } else {
+                                        return "white";
+                                    }
+                                });
+                            }
+                        }
+                        return tooltip.style("visibility", "hidden");
+                    })
+                    .style("stroke", function (d) {
                         return "white";
-                    }
+                    });
                 });
-                if (msa_data.hasOwnProperty(d.properties.GEOID)) {
-                    return tooltip.style("visibility", "visible").text(msa_data[d.properties.GEOID]["name"] + ": " + msa_data[d.properties.GEOID]["net"]);
-                }
-            })
-            .on("mousemove", function(){
-                return tooltip.style("top", (d3.event.pageY-10)+"px").style("left",(d3.event.pageX+10)+"px");
-            })
-            .on("mouseout", function(d) {
-                d3.select(this).style("fill", function (d) {
-                    if (msa_data.hasOwnProperty(d.properties.GEOID)) {
-                        return "lightgray";
-                    } else {
-                        return "white";
-                    }
-                });
-                return tooltip.style("visibility", "hidden");
-            })
-            .style("stroke", function (d) {
-                return "white";
             });
         });
     });
