343ff3e805ad2dd1fa937e634776fab792ed76c4
Dorrie Tang <dorrie.tang@gmail.com>
2019-12-11 17:36:27 -0500

    Add retire

--- d3charts.js
+++ d3charts.js
@@ -333,6 +333,158 @@ function outliers() {
     });
 }
 
+function retire() {
+    const path = d3.geoPath(d3.geoAlbersUsa());
+
+    var svg = d3.select(".sticky1").append("svg")
+        .attr("width", 900)
+        .attr("height", 500);
+
+    let g = svg.append("g");
+
+    d3.json(map_url).then(function(map) {
+        d3.json(data_url).then(function(data) {
+            d3.json(coords_url).then(function(coords) {
+                d3.json(connections_url).then(function(conns) {
+                    // Bind metro paths
+                    g.selectAll("path")
+                    .data(topojson.feature(map, map.objects.cb_2014_us_cbsa_500k).features)
+                    .enter().append("path")
+                    .attr("d", path)
+                    .style("fill", function (d) {
+                        var curr_msa = d.properties.GEOID;
+                        cans[curr_msa] = [this.getPointAtLength(this.getTotalLength()).x, this.getPointAtLength(this.getTotalLength()).y];
+                        
+                        if (data.hasOwnProperty(d.properties.GEOID)) {
+                            return "lightgray";
+                        } else {
+                            return "white";
+                        }
+                    })
+                    // .on("mouseout", function(d) {
+                    //     var curr_msa = d.properties.GEOID;
+                    //     d3.selectAll("line").remove();
+                    //     d3.select(this).style("fill", function (d) {
+                    //         if (data.hasOwnProperty(curr_msa)) {
+                    //             return "lightgray";
+                    //         } else {
+                    //             return "white";
+                    //         }
+                    //     });
+                    //     for (var i in conns[curr_msa]) {
+                    //         if (conns[curr_msa][i]["dest"] in coords) {
+                    //             // stroke around dests
+                    //             d3.selectAll('path').style("fill", function (e) {
+                    //                 if (data.hasOwnProperty(e.properties.GEOID)) {
+                    //                     return "lightgray";
+                    //                 } else {
+                    //                     return "white";
+                    //                 }
+                    //             });
+                    //         }
+                    //     }
+                    //     d3.select('#dest_tt_1').style("visibility", "hidden");
+                    //     d3.select('#dest_tt_2').style("visibility", "hidden");
+                    //     d3.select('#dest_tt_3').style("visibility", "hidden");
+                    //     d3.select('#dest_tt_4').style("visibility", "hidden");
+                    //     return tooltip.style("visibility", "hidden");
+                    // })
+                    .style("stroke", function (d) {
+                        return "white";
+                    });
+
+                    var places = ["32840", "24540", "45540"];
+                    for (var x in places) {
+                        var curr_msa = places[x];
+                        // draw lines from source to dests
+                        for (var i in conns[curr_msa]) {
+                            var dest_msa = conns[curr_msa][i]["dest"];
+                            if (dest_msa in coords) {
+                                // draw line
+                                svg.append("line")
+                                .attr("x1", cans[curr_msa][0])
+                                .attr("y1", cans[curr_msa][1])
+                                .attr("x2", cans[dest_msa][0])
+                                .attr("y2", cans[dest_msa][1])
+                                .attr("stroke-width", Math.abs(parseInt(conns[dest_msa][i]["value"]))/5000)
+                                .attr("stroke", function() {
+                                    if (parseInt(conns[curr_msa][i]["value"]) < 0) {
+                                        return "red";
+                                    } else {
+                                        return "lightgreen";
+                                    }
+                                });
+                            }
+                        }
+                    }
+                    
+                    // blacken dests
+                    d3.selectAll('path').style("fill", function (e) {
+                        // highlight texas
+                        for (var x in places) {
+                            var curr_msa = places[x];
+                            if (e.properties.GEOID == curr_msa) {
+                                return "black";
+                            }
+                            // highlight dests
+                            for (var i in conns[curr_msa]) {
+                                var dest_msa = conns[curr_msa][i]["dest"];
+                                if (dest_msa in coords) {
+                                    // if (data.hasOwnProperty(dest_msa)) {
+                                    //     if (i == 0) {
+                                    //         d3.select('#dest_tt_1').style("visibility", "visible").html(data[dest_msa]["name"] + "<br> Number of migrants: " + conns[curr_msa][i]["value"] + "<br> RPI: " + data[dest_msa]["rpi"]);
+                                    //     }
+                                    //     if (i == 1) {
+                                    //         d3.select('#dest_tt_2').style("visibility", "visible").html(data[dest_msa]["name"] + "<br> Number of migrants: " + conns[curr_msa][i]["value"] + "<br> RPI: " + data[dest_msa]["rpi"]);
+                                    //     }
+                                    //     if (i == 2) {
+                                    //         d3.select('#dest_tt_3').style("visibility", "visible").html(data[dest_msa]["name"] + "<br> Number of migrants: " + conns[curr_msa][i]["value"] + "<br> RPI: " + data[dest_msa]["rpi"]);
+                                    //     }
+                                    //     if (i == 3) {
+                                    //         d3.select('#dest_tt_4').style("visibility", "visible").html(data[dest_msa]["name"] + "<br> Number of migrants: " + conns[curr_msa][i]["value"] + "<br> RPI: " + data[dest_msa]["rpi"]);
+                                    //     }
+                                    // }
+                                    if (e.properties.GEOID == dest_msa) {
+                                        return "gray";
+                                    }
+                                }
+                            }
+                        }
+                        if (data.hasOwnProperty(e.properties.GEOID)) {
+                            return "lightgray";
+                        } else {
+                            return "white";
+                        }
+                    });
+                    
+                    // show tooltip
+                    // tooltip.style("visibility", "visible").html(data[curr_msa]["name"] + "<br> Net population change, 2013-2017: " + data[curr_msa]["net"] + "<br> RPI: " + data[curr_msa]["rpi"]);
+                    
+
+                    // for (var i in conns[curr_msa]) {
+                    //     var dest_msa = conns[curr_msa][i]["dest"];
+                    //     if (dest_msa in coords) {
+                    //         if (i == 0) {
+                    //             d3.select('#dest_tt_1').style("top", (cans[dest_msa][1]+d3.event.pageY-cans[curr_msa][1]-10)+"px").style("left",(cans[dest_msa][0]+410)+"px");
+                    //         }
+                    //         if (i == 1) {
+                    //             d3.select('#dest_tt_2').style("top", (cans[dest_msa][1]+d3.event.pageY-cans[curr_msa][1]-10)+"px").style("left",(cans[dest_msa][0]+410)+"px");
+                    //         }
+                    //         if (i == 2) {
+                    //             d3.select('#dest_tt_3').style("top", (cans[dest_msa][1]+d3.event.pageY-cans[curr_msa][1]-10)+"px").style("left",(cans[dest_msa][0]+410)+"px");
+                    //         }
+                    //         if (i == 3) {
+                    //             d3.select('#dest_tt_4').style("top", (cans[dest_msa][1]+d3.event.pageY-cans[curr_msa][1]-10)+"px").style("left",(cans[dest_msa][0]+410)+"px");
+                    //         }
+                    //     }
+                    // }
+                    // return tooltip.style("top", (d3.event.pageY-10)+"px").style("left",(d3.event.pageX+10)+"px");
+                });
+            });
+        });
+    });
+}
+
 function texas() {
     var texas_msa = "33260";
 
@@ -523,6 +675,11 @@ function handleStepEnter(response) {
     if (response.index == 2){
         d3.select('.sticky1 svg').remove();
         outliers();
+    }
+
+    if (response.index == 3){
+        d3.select('.sticky1 svg').remove();
+        retire();
     }
     
     if (response.index == 4) {
