dde49429609356ec6edc1da3bf68890be966b70a
Dorrie Tang <dorrie.tang@gmail.com>
2019-12-11 03:26:06 -0500

    Add d3 svg

--- d3charts.js
+++ d3charts.js
@@ -1,3 +1,72 @@
+const map_url = "https://raw.githubusercontent.com/erickawu/scroll_narrative/master/msa_map.json";
+const data_url = "https://raw.githubusercontent.com/erickawu/scroll_narrative/master/msa_data.json";
+
+var path = d3.geoPath(d3.geoEquirectangular());
+
+var tooltip = d3.select("body").append("div")
+  .attr("class", "tooltip") // need to write html to style this box
+  .style("position", "absolute")
+  .style("visibility", "hidden");
+
+function map() {
+    const path = d3.geoPath(d3.geoAlbersUsa());
+
+    var svg = d3.select(".sticky1").append("svg")
+        .attr("width", 900)
+        .attr("height", 500);
+
+    let g = svg.append("g");
+
+    d3.json(map_url).then(function(MSA) {
+        d3.json(data_url).then(function(msa_data) {
+            // Bind metro paths
+            g.selectAll("path")
+            .data(topojson.feature(MSA, MSA.objects.cb_2014_us_cbsa_500k).features)
+            .enter().append("path")
+            .attr("d", path)
+            .style("fill", function (d) {
+                if (msa_data.hasOwnProperty(d.properties.GEOID)) {
+                    return "white";
+                } else {
+                    return "gray";
+                }
+            })
+            .on("mouseover", function(d) {
+                d3.select(this).style("fill", function (d) {
+                    if (msa_data.hasOwnProperty(d.properties.GEOID) && msa_data[d.properties.GEOID].hasOwnProperty("net")) {
+                        return d3.interpolatePiYG((msa_data[d.properties.GEOID]["net"] + 149227) / 298454);
+                    } else {
+                        return "gray";
+                    }
+                });
+                if (msa_data.hasOwnProperty(d.properties.GEOID)) {
+                    return tooltip.style("visibility", "visible").text(msa_data[d.properties.GEOID]["name"] + ": " + msa_data[d.properties.GEOID]["net"]);
+                }
+            })
+            .on("mousemove", function(){
+                return tooltip.style("top", (d3.event.pageY-10)+"px").style("left",(d3.event.pageX+10)+"px");
+            })
+            .on("mouseout", function(d) {
+                d3.select(this).style("fill", function (d) {
+                    if (msa_data.hasOwnProperty(d.properties.GEOID) && msa_data[d.properties.GEOID].hasOwnProperty("net")) {
+                        return "white";
+                    } else {
+                        return "gray";
+                    }
+                });
+                return tooltip.style("visibility", "hidden");
+            })
+            .style("stroke", function (d) {
+                if (msa_data.hasOwnProperty(d.properties.GEOID)) {
+                    return "lightgray";
+                } else {
+                    return "gray";
+                }
+            });
+        });
+    });
+}
+
 // using d3 for convenience
 var main = d3.select('main')
 var scrolly = main.select('#scrolly');
@@ -65,6 +134,8 @@ function init() {
 
     // setup resize event
     window.addEventListener('resize', handleResize);
+
+    map();
 }
 
 // kick things off
