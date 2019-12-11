3d5f58bcf92a3dc236423aaaf0f5361fc29b1f44
Ericka Wu <ew2575@columbia.edu>
2019-12-11 14:55:56 -0500

    scatter

--- d3charts.js
+++ d3charts.js
@@ -1,12 +1,85 @@
 const map_url = "https://raw.githubusercontent.com/erickawu/scroll_narrative/master/msa_map.json";
 const data_url = "https://raw.githubusercontent.com/erickawu/scroll_narrative/master/msa_data.json";
-
+const scatter_url = "https://raw.githubusercontent.com/erickawu/scroll_narrative/master/perc_change.json";
+const dict = ["11640", "41980", "10380", "25020", "41900", "32420", "38660", "24540", "34820", "45540", "33260"];
 var path = d3.geoPath(d3.geoEquirectangular());
 
 var tooltip = d3.select("body").append("div")
   .attr("class", "tooltip") // need to write html to style this box
   .style("position", "absolute")
   .style("visibility", "hidden");
+
+var y = d3.scaleLinear()
+    .domain([5,-4])
+    .range([90, 440])
+
+var x = d3.scaleLinear()
+    .domain([0,120000])
+    .range([80,770])
+
+var yAxis = g => g
+    .attr("transform", `translate(${80}, 0)`)
+    .call(d3.axisLeft(y));
+
+var xAxis = g => g
+    .attr("transform", `translate(0, ${440})`)
+    .call(d3.axisBottom(x));
+
+function scatterplot() {
+   var svg = d3.select(".sticky1").append("svg")
+        .attr("width", 850)
+        .attr("height", 500);
+
+  d3.json(scatter_url).then(function(scatter) {
+      svg.append("g")
+        .selectAll("g")
+        .data(scatter)
+        .join("g")
+          .attr("transform", d => `translate(${x(d.rpi)},${y(d.perc_change)})`)
+          .call(g => g.append("circle")
+              .attr("stroke", "gray")
+              .attr("fill", "gray")
+              .attr("opacity", 0.5)
+              .attr("r", 2.5)
+           .on("mouseover", function(d) {
+            d3.select(this).style("r", "6");
+            return tooltip.style("visibility", "visible").text(d.origin);
+          })
+          .on("mousemove", function(){
+            return tooltip.style("top", (d3.event.pageY-10)+"px").style("left",(d3.event.pageX+10)+"px");
+          })
+          .on("mouseout", function(d) {
+            d3.select(this).style("stroke", "gray").style("r", "2.5");
+            return tooltip.style("visibility", "hidden");
+          }))
+          .call(g => g.append("circle")
+              .filter(function(d) {return dict.includes(d.msa_num)})
+              .attr("fill", "none")
+              .attr("stroke", "red")
+              .attr("r", 20)
+              .attr("opacity", "0.5")
+              .on("mouseover", function(d) {
+            d3.select(this).style("fill", "red").style("opacity", "0.5");
+            return tooltip.style("visibility", "visible").text(d.origin);
+          })
+              .on("mousemove", function(){
+            return tooltip.style("top", (d3.event.pageY-10)+"px").style("left",(d3.event.pageX+10)+"px");
+          })
+              .on("mouseout", function(d) {
+            d3.select(this).style("fill", "none").style("opacity", "0.5");
+            return tooltip.style("visibility", "hidden");
+          }))
+      });
+      
+  
+  svg.append("g")
+      .call(xAxis);
+
+  svg.append("g")
+      .call(yAxis);
+  
+  return svg.node();
+}   
 
 function map() {
     const path = d3.geoPath(d3.geoAlbersUsa());
@@ -95,11 +168,13 @@ function handleStepEnter(response) {
     console.log(response)
     // response = { element, direction, index }
 
-    if (response.direction == 'up' && response.index == 0){
+    if (response.index == 0){
 		d3.select('.sticky1 svg').remove();
-	}
-    if (response.direction == 'down' && response.index == 1){
-		map();
+        scatterplot();
+	}  
+    if (response.index == 1){
+		d3.select('.sticky1 svg').remove();
+        map();
 	}
 
     // add color to current step only
